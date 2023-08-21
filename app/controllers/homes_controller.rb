49cc27394849732bc4b69dfb1c0b8e9bc372a563
EC2 Default User <ec2-user@ip-172-31-6-109.ap-northeast-1.compute.internal>
2023-08-21 05:40:48 +0000

    202308211440

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,28 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/new'
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/edit'
+  get 'homes/create'
+  get 'homes/edit'
+  get 'homes/update'
+  get 'homes/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
